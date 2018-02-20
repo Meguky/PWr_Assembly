ab3725f2ecc7cc217bcf308b00ddebcdcd44b508
Meguky <kynio1997@gmail.com>
2018-02-20 12:19:21 +0100

    Zad0 - Added user input

--- L1/zad0.s
+++ L1/zad0.s
@@ -5,18 +5,30 @@ STDOUT = 1
 STDIN = 0
 SYS_EXIT = 60
 SYS_SUCCESS = 0
+BUF_LENGTH = 512
 
-buf: .ascii "Hello, World!\n"
+buf: .ascii "Hello, World!"
 buf_len = .-buf
 
+.bss
+.lcomm buf_read, 512
+
+.text
 .globl _start
 
 _start:
+nop
+movq $SYSREAD, %rax
+movq $STDIN, %rdi
+movq $buf_read, %rsi
+movq $BUF_LENGTH, %rdx
+syscall
+
+movq %rax, %rdx
 
 movq $SYSWRITE, %rax
 movq $STDOUT, %rdi
-movq $buf, %rsi
-movq $buf_len, %rdx
+movq $buf_read, %rsi
 syscall
 
 movq $SYS_EXIT, %rax

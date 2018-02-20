59e9b53dd589bd8613a8363b8a6f90eac0372f28
Meguky <kynio1997@gmail.com>
2018-02-20 11:26:03 +0100

    Zad0 - Hello world

--- L1/zad0.s
+++ L1/zad0.s
@@ -1,3 +1,24 @@
 .data
+SYSWRITE = 1
+SYSREAD = 0
+STDOUT = 1
+STDIN = 0
+SYS_EXIT = 60
+SYS_SUCCESS = 0
 
+buf: .ascii "Hello, World!\n"
+buf_len = .-buf
 
+.globl _start
+
+_start:
+
+movq $SYSWRITE, %rax
+movq $STDOUT, %rdi
+movq $buf, %rsi
+movq $buf_len, %rdx
+syscall
+
+movq $SYS_EXIT, %rax
+movq $SYS_SUCCESS, %rdi
+syscall
